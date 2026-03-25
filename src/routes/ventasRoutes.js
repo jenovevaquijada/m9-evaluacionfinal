f7f01eb53b3d5c8f66a342a78abdfe48c79815a2
Jenoveva Quijada <jenovevaquijada@gmail.com>
2026-03-25 19:49:47 -0300

    Delete src directory

--- src/routes/ventasRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { procesarCompra, obtenerHistorial } = require('../controllers/ventasController');
-const verificarToken = require('../middleware/authMiddleware');
-
-router.post('/', verificarToken, procesarCompra);
-router.get('/usuario', verificarToken, obtenerHistorial);
-
-module.exports = router;
